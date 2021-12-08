35d4fc8be2cc2b238bb3a31732a04d6cc8d8530a
Superlee506 <superli@zju.edu.cn>
2021-12-08 17:04:12 +0800

    seperate statement and implement

---
+++ include/nori/sampler/independentSampler.h
@@ -0,0 +1,45 @@
+/*
+    This file is part of Nori, a simple educational ray tracer
+
+    Copyright (c) 2015 by Wenzel Jakob
+*/
+
+#include <nori/core/sampler.h>
+#include <pcg32.h>
+
+NORI_NAMESPACE_BEGIN
+
+/**
+ * Independent sampling - returns independent uniformly distributed
+ * random numbers on <tt>[0, 1)x[0, 1)</tt>.
+ *
+ * This class is essentially just a wrapper around the pcg32 pseudorandom
+ * number generator. For more details on what sample generators do in
+ * general, refer to the \ref Sampler class.
+ */
+class IndependentSampler : public Sampler {
+public:
+    IndependentSampler(const PropertyList &propList);
+
+    virtual ~IndependentSampler();
+
+    virtual std::unique_ptr<Sampler> clone() const override;
+
+    virtual void prepare(const ImageBlock &block) override;
+
+    virtual void generate() override;
+    virtual void advance() override;
+
+    virtual float next1D() override;
+
+    virtual Point2f next2D() override;
+
+    virtual std::string toString() const override;
+protected:
+    IndependentSampler();
+
+private:
+    pcg32 m_random;
+};
+
+NORI_NAMESPACE_END
---
+++ src/sampler/independentSampler.cpp
@@ -0,0 +1,64 @@
+/*
+    This file is part of Nori, a simple educational ray tracer
+
+    Copyright (c) 2015 by Wenzel Jakob
+*/
+
+#include <nori/sampler/independentSampler.h>
+#include <nori/core/block.h>
+#include <pcg32.h>
+
+NORI_NAMESPACE_BEGIN
+
+IndependentSampler::IndependentSampler(const PropertyList &propList)
+{
+    m_sampleCount = (size_t) propList.getInteger("sampleCount", 1);
+}
+
+IndependentSampler::~IndependentSampler()
+{
+
+}
+
+std::unique_ptr<Sampler> IndependentSampler::clone() const
+{
+    std::unique_ptr<IndependentSampler> cloned(new IndependentSampler());
+    cloned->m_sampleCount = m_sampleCount;
+    cloned->m_random = m_random;
+    return std::move(cloned);
+}
+
+void IndependentSampler::prepare(const ImageBlock &block)
+{
+    m_random.seed(
+            block.getOffset().x(),
+            block.getOffset().y()
+    );
+}
+
+void IndependentSampler::generate() { /* No-op for this sampler */ }
+void IndependentSampler::advance()  { /* No-op for this sampler */ }
+
+float IndependentSampler::next1D()
+{
+    return m_random.nextFloat();
+}
+
+Point2f IndependentSampler::next2D()
+{
+    return Point2f(
+            m_random.nextFloat(),
+            m_random.nextFloat()
+    );
+}
+
+std::string IndependentSampler::toString() const
+{
+    return tfm::format("Independent[sampleCount=%i]", m_sampleCount);
+}
+
+IndependentSampler::IndependentSampler() { }
+
+
+NORI_REGISTER_CLASS(IndependentSampler, "independent");
+NORI_NAMESPACE_END
